78a6716fcf73d2633e25e6ea6fd842a72f9c9c08
marcel <filej1025@gmail.com>
2020-10-26 16:19:00 +0100

    getting user input

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-    cout << pow(2, 5) << endl; // will return 2 to the power of 5 -----> 32
-    cout << sqrt(36) << endl; // will return square root of 36 -----> 6
-    cout << round(4.6) << endl; // will return rounded number -----> 5
-    cout << ceil(5.2) << endl; // will always return number rounded up ----> 6
-    cout << floor(3.8) << endl; // will always return number rounded down ----> 3
+ //   int age;
+ //   cout << "Enter your age: ";
+ //   cin >> age;
+
+ //   cout << "Your are " << age << " years old." << endl;
+
+
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name); // getting and storing whole line of text instead of one charakter or intiger 
+
+    cout << "Hello " << name << endl;
+
 
     return 0;
 }
