b299c891b661ec9420b2004f371e952fb99cf2b1
marcel <filej1025@gmail.com>
2020-10-26 16:06:42 +0100

    working with numbers

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
 
-    string phrase = "Giraffe Academy";
-    string phrasesub;
-    phrase[0] = 'B';
-    phrasesub = phrase.substr(8, 3);
-    cout << phrasesub << endl;
+    cout << pow(2, 5) << endl; // will return 2 to the power of 5 -----> 32
+    cout << sqrt(36) << endl; // will return square root of 36 -----> 6
+    cout << round(4.6) << endl; // will return rounded number -----> 5
+    cout << ceil(5.2) << endl; // will always return number rounded up ----> 6
+    cout << floor(3.8) << endl; // will always return number rounded down ----> 3
 
     return 0;
 }
