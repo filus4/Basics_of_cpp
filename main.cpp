5d05bd9283fd651074ef753e197c212a0c222d9a
marcel <filej1025@gmail.com>
2020-10-27 13:34:42 +0100

    while and do while loop

--- main.cpp
+++ main.cpp
@@ -2,41 +2,18 @@
 
 using namespace std;
 
-string get_day_of_week(int day_num) {
-    string day_name;
-
-    switch(day_num) {
-        case 0:
-            day_name = "Sunday";
-            break;
-        case 1:
-            day_name = "Monday";
-            break;
-        case 2:
-            day_name = "Tuesday";
-            break;
-        case 3:
-            day_name = "Wednesday";
-            break;
-        case 4:
-            day_name = "Thursday";
-            break;
-        case 5:
-            day_name = "Friday";
-            break;
-        case 6:
-            day_name = "Saturday";
-            break;
-        default:
-        day_name = "Invalid Day Number";
-    }
-
-    return day_name;
-}
-
 int main() {
 
-    cout << get_day_of_week(12);
+    int index = 6;
+    while(index <= 5) {
+        cout << index << endl;
+        index++;
+    }
+
+    do { 
+        cout << index << endl;
+        index++;
+    } while(index <= 5);
 
     return 0;
 }
