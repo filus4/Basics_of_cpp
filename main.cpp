43c264885c3d6590c07ef87c7fb39306096241eb
marcel <filej1025@gmail.com>
2020-10-27 15:30:45 +0100

    comments

--- main.cpp
+++ main.cpp
@@ -6,18 +6,14 @@ using namespace std;
 
 int main() {
 
-int number_grid[3][2] = {
-{1,2},
-    {3,4},
-        {5,6}
-    };
+    // Comment
+    cout << "Comments are fun";
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            cout << number_grid[i][j];
-        }
-        cout << endl;
-    }
-
+    /* 
+    Comment
+    Comment
+    Comment
+    */
+   
     return 0;
 }
