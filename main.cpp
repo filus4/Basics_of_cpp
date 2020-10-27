936ff13aca119d0258d3df8bc437eeb43704321e
marcel <filej1025@gmail.com>
2020-10-27 15:15:42 +0100

    exponent function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
+int power(int base_num, int pow_num) {
+    int result = 1;
+
+    for (int i = 0; i < pow_num; i++) {
+        result *= base_num;
+    }
+
+    return result;
+}
+
 int main() {
 
-    int nums[] = {1, 2, 5, 7, 3};
-    for (int i = 0; i < 5; i++) {
-        cout << nums[i] << endl;
-    }
-    
+    cout << power(4, 2);   
+
     return 0;
 }
