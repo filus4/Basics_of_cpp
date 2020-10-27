5bd77f83f0e148e2e00c1c02bec40abf1dd62cb1
marcel <filej1025@gmail.com>
2020-10-27 15:46:51 +0100

    pointers

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include <array>
 
 using namespace std;
 
+/*
+    cout << "Age: " << &age << endl;
+    cout << "Gpa: " << &gpa << endl;
+    cout << "Name: " << &name << endl;
+*/
 
 int main() {
 
-    // Comment
-    cout << "Comments are fun";
+    int age = 19;
+    int *pAge = &age; // storing a pointer in variable
 
-    /* 
-    Comment
-    Comment
-    Comment
-    */
-   
+    double gpa = 2.7;
+    double *pGpa = &gpa;
+
+    string name = "Mike";
+    string *pName = &name;
+
+    cout << pAge << endl; 
+
+    cout << *pAge << endl; // dereferencing pointer
+    cout << *&gpa; // its possible to join * and & together
+
     return 0;
 }
